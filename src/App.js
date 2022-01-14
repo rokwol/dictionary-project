bd8fd552738e28683611032e1803711f36e04795
rokwol <roksolanawoloszyn@gmail.com>
2022-01-14 17:09:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import logo from "./rw-logo.png";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="rw-logo" alt="logo"></img>
         <h1> Hello</h1>
         <h2>dictionary time</h2>
       </header>

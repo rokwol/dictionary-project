ff579080bd654f54e9dd6232d6b6bb136300c345
rokwol <roksolanawoloszyn@gmail.com>
2022-01-16 23:17:17 -0500

    Added phonetic component which returns phonetic audio and text

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning";
 import "./Results.css";
 
@@ -8,6 +9,15 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2 className="word"> {props.results.word}</h2>
+
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
